686887ed42186c025d46a391e69e2c959cca9bfd
Evan Rybovic <rybovic.evan@gmail.com>
2018-12-15 23:12:08 -0800

    Added Anko to easily make async functions. Made the centering on the first location happen asynchronously so the map will load and move when the geocode is parsed instead of the second activity hanging then having the map load in the desired location

--- app/src/main/java/com/example/evanr/notgooglemaps/MapsActivity.kt
+++ app/src/main/java/com/example/evanr/notgooglemaps/MapsActivity.kt
@@ -12,6 +12,8 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.uiThread
 import java.util.*
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
@@ -41,16 +43,28 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         // Geocoder tanslates an address to a latitude and longitude location
         val geocoder = Geocoder(this, Locale.getDefault())
 
-        // getFromLocation returns in json format
-        val addressList = geocoder.getFromLocationName(firstLocation, 1)
-        if (addressList.size > 0) {
-            // get the first entry...also its the only entry
-            val address = addressList.get(0)
-            val latLngOrigin = LatLng(address.latitude, address.longitude)
-            mMap!!.moveCamera(CameraUpdateFactory.newLatLngZoom(latLngOrigin, 14.5f))
+        // Run the geocoder api in seperate thread
+        doAsync{
+            // getFromLocation returns in json format
+            val addressList = geocoder.getFromLocationName(firstLocation, 1)
+            if (addressList.size > 0) {
+                // get the first entry...also its the only entry
+                val address = addressList.get(0)
+                val latLngOrigin = LatLng(address.latitude, address.longitude)
+
+                // Threadsafe call to move the map
+                uiThread {
+                    Toast.makeText(this@MapsActivity, "Centering on First Location", Toast.LENGTH_LONG ).show()
+                    mMap!!.moveCamera(CameraUpdateFactory.newLatLngZoom(latLngOrigin, 14.5f))
+                }
+            }
+            else{
+                // Threadsafe call to a Toast
+                uiThread{
+                    Toast.makeText(this@MapsActivity, "Could not parse address", Toast.LENGTH_LONG).show()
+                }
+            }
         }
-        else{
-            Toast.makeText(this, "Could not parse address", Toast.LENGTH_LONG).show()
-        }
+
     }
 }
