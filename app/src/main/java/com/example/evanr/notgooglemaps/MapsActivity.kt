0ecdcd365907f278deb042808b7b269eab5f0c5e
Evan Rybovic <rybovic.evan@gmail.com>
2018-12-15 21:27:41 -0800

    Implemented Geocoding to get the latitude and longitude of the first address. Also the map will center to that location if the address is valid.

--- app/src/main/java/com/example/evanr/notgooglemaps/MapsActivity.kt
+++ app/src/main/java/com/example/evanr/notgooglemaps/MapsActivity.kt
@@ -3,6 +3,8 @@ package com.example.evanr.notgooglemaps
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import android.location.Geocoder
+import android.util.Log
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -10,6 +12,7 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import java.util.*
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
@@ -25,23 +28,29 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
 
+
+        // Get the data from the intent
         val intent = intent
         firstLocation = intent.getStringExtra("firstLocation")
         secondLocation = intent.getStringExtra("secondLocation")
-
-        Toast.makeText(this, firstLocation + " "+ secondLocation, Toast.LENGTH_LONG).show()
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
+
+        // Geocoder tanslates an address to a latitude and longitude location
+        val geocoder = Geocoder(this, Locale.getDefault())
+
+        // getFromLocation returns in json format
+        val addressList = geocoder.getFromLocationName(firstLocation, 1)
+        if (addressList.size > 0) {
+            // get the first entry...also its the only entry
+            val address = addressList.get(0)
+            val latLngOrigin = LatLng(address.latitude, address.longitude)
+            mMap!!.moveCamera(CameraUpdateFactory.newLatLngZoom(latLngOrigin, 14.5f))
+        }
+        else{
+            Toast.makeText(this, "Could not parse address", Toast.LENGTH_LONG).show()
+        }
     }
 }
